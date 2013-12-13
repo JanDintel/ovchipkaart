223555d68afae9b3598dbe14eeb120ee6d905658
Jan van der Pas <janvanderpas@gmail.com>
2013-12-14 00:31:28 +0100

    Add Capybara DSL with chromedriver to Scraper

--- lib/ovchipkaart/scraper.rb
+++ lib/ovchipkaart/scraper.rb
@@ -1,2 +1,17 @@
+require 'capybara'
+require 'capybara/dsl'
+
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.default_driver = :selenium_chrome
+
 class Scraper
+  include Capybara::DSL
+
+  def visit_ovchipkaart
+    visit('https://www.ov-chipkaart.nl/')
+    page.save_screenshot('tmp/screenshot.png')
+  end
 end
--- spec/lib/scraper_spec.rb
+++ spec/lib/scraper_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
 describe Scraper do
+
+  describe '#visit_ovchipkaart' do
+    specify { described_class.new.visit_ovchipkaart }
+  end
 end
