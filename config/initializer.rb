fb96276e7a2d95c9a722e582b45c24ab7d6bd2a7
Jan van der Pas <janvanderpas@gmail.com>
2013-12-14 03:17:58 +0100

    Add download directory to Capybara Chrome driver

--- config/initializer.rb
+++ config/initializer.rb
@@ -4,9 +4,13 @@ require 'yaml'
 # Capybara
 require 'capybara'
 require 'capybara/dsl'
+require 'selenium/webdriver'
 
 Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  preferences = {"download" => {"default_directory" => Dir.getwd + '/tmp/downloads', "directory_upgrade" => true, "extensions_to_open" => ""}}
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome
+  capabilities['chromeOptions'] = {'prefs' => preferences}
+  Capybara::Selenium::Driver.new(app, :detach => false, :browser => :chrome, :desired_capabilities => capabilities)
 end
 
 Capybara.default_driver = :selenium_chrome
