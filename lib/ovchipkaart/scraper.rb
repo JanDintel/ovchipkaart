5c5fdffb342155112a2b18e7cf1e9cf17e6698b6
Jan van der Pas <janvanderpas@gmail.com>
2013-12-14 01:56:37 +0100

    Add visit root page, login user and download transaction history interaction with the ovchipkaart website

--- lib/ovchipkaart/scraper.rb
+++ lib/ovchipkaart/scraper.rb
@@ -11,7 +11,39 @@ class Scraper
   include Capybara::DSL
 
   def visit_ovchipkaart
-    visit('https://www.ov-chipkaart.nl/')
-    page.save_screenshot('tmp/screenshot.png')
+    visit('https://www.ov-chipkaart.nl/login/')
+    page.save_screenshot('tmp/screenshot1.png')
+    accept_cookies if need_to_accept_cookies?
+    self
+  end
+
+  def login_user
+    fill_in 'gebruikersnaam', with: Ovchipkaart.config[:username]
+    fill_in 'wachtwoord',     with: Ovchipkaart.config[:password]
+    click_button 'Inloggen'
+    page.save_screenshot('tmp/screenshot2.png')
+    self
+  end
+
+  def visit_transaction_overview
+    click_link 'Transactieoverzicht'
+    page.save_screenshot('tmp/screenshot3.png')
+    self
+  end
+
+  def download_transaction_history
+    click_link 'Opslaan als CSV'
+    page.save_screenshot('tmp/screenshot4.png')
+    self
+  end
+
+  private
+
+  def need_to_accept_cookies?
+    !find_link('Ik accepteer deze cookies').nil?
+  end
+
+  def accept_cookies
+    click_on('Ik accepteer deze cookies')
   end
 end
--- spec/lib/scraper_spec.rb
+++ spec/lib/scraper_spec.rb
@@ -1,8 +1,33 @@
 require 'spec_helper'
+require 'capybara/rspec'
+
+# The scraper methods can't run independently
+RSpec.configure { |config| config.order = 'default' }
 
 describe Scraper do
+  subject { described_class.new }
 
   describe '#visit_ovchipkaart' do
-    specify { described_class.new.visit_ovchipkaart }
+    it 'lands on the login page of ovchipkaart' do
+      expect(subject.visit_ovchipkaart).to have_content 'Inloggen op Mijn OV-chipkaart'
+    end
+  end
+
+  describe '#login_user' do
+    it 'fills out the user credentials and logs in' do
+      expect(subject.login_user).to have_content 'Transactieoverzicht'
+    end
+  end
+
+  describe '#visit_transaction_overview' do
+    it 'visits the transaction overview page' do
+      expect(subject.visit_transaction_overview).to have_content 'Mijn transactieoverzicht'
+    end
+  end
+
+  describe '#download_transaction_history' do
+    it 'downloads the transaction history' do
+      expect(subject.download_transaction_history).to have_content 'foo'
+    end
   end
 end
