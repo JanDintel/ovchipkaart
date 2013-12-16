4e378e22c26e1ab51c879c5ba9524664d28eda9e
Jan van der Pas <janvanderpas@gmail.com>
2013-12-16 22:36:59 +0100

    Add the processing of the downloaded transactions

--- lib/ovchipkaart/parser.rb
+++ lib/ovchipkaart/parser.rb
@@ -1,3 +1,53 @@
+require 'csv'
+
 class Parser
+  attr_reader :journeys, :check_ins, :additions, :others, :products
+
+  def initialize
+    @journeys   = []
+    @check_ins  = []
+    @additions  = []
+    @others     = []
+    @products   = []
+  end
+
+  def self.process_transactions
+    new.sort_csv_file
+  end
+
+  def sort_csv_file
+    transactions.each do |row|
+      transactie = row[:transactie]
+      if transactie     == "Reis"
+        journeys << row
+      elsif transactie  == "Check-in"
+        check_ins << row
+      elsif transactie  == "Saldo opgeladen"
+        additions << row
+      elsif transactie  == "Product op kaart gezet"
+        products << row
+      else
+        others << row
+      end
+    end
+  end
+
+  def parse_csv_file
+    CSV.new(csv_file, headers: true, header_converters: :symbol, col_sep: ';')
+  end
+
+  def csv_file
+    File.read last_downloaded_file
+  end
+
+  def last_downloaded_file
+    Dir['tmp/downloads/*'].sort_by{ |f| File.basename(f) }.first
+  end
+
+  private
+
+  def transactions
+    @transactions ||= parse_csv_file.to_a.map { |row| row.to_hash }
+  end
 
 end
--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,4 +1,59 @@
 require 'spec_helper'
 
 describe Parser do
+  let(:csv_file_path) { 'spec/fixtures/test_transaction_1.csv' }
+  let(:csv_file)      { File.read 'spec/fixtures/test_transaction_1.csv' }
+  let(:csv_file_stub) { described_class.any_instance.stub(:csv_file).and_return csv_file }
+
+  describe '.process_transactions' do
+    it 'sorts the transactions' do
+      expect_any_instance_of(described_class).to receive :sort_csv_file
+      described_class.process_transactions
+    end
+  end
+
+  describe '#sort_csv_file' do
+    before  { csv_file_stub }
+    before  { subject.sort_csv_file }
+
+    specify { expect(subject.journeys.count).to   eql 4 }
+    specify { expect(subject.check_ins.count).to  eql 6 }
+    specify { expect(subject.additions.count).to  eql 1 }
+    specify { expect(subject.products.count).to   eql 1 }
+    specify { expect(subject.others.count).to     eql 0 }
+  end
+
+  describe '#parse_csv_file' do
+    before  { csv_file_stub }
+
+    it 'parses the csv file' do
+      expect(CSV).to receive(:new).with(csv_file, { :headers=>true, :header_converters=>:symbol, :col_sep=>";" })
+      subject.parse_csv_file
+    end
+  end
+
+  describe 'downloaded csv file' do
+    let(:file_1_path) { Dir.getwd + '/tmp/downloads/transacties_-0001.csv' }
+    let(:file_2_path) { Dir.getwd + '/tmp/downloads/transacties_-0002.csv' }
+
+    before do
+      File.open(file_1_path, 'w+') { |file| file.write csv_file }
+      File.new file_2_path, 'w+'
+    end
+
+    after do
+      FileUtils.rm file_1_path
+      FileUtils.rm file_2_path
+    end
+
+    context '#csv_file' do
+      specify { expect(subject.csv_file).to eql csv_file }
+    end
+
+    context '#last_downloaded_file' do
+      it 'sorts the file on their basename and returns first one' do
+        expect(subject.last_downloaded_file).to eql 'tmp/downloads/transacties_-0001.csv'
+      end
+    end
+  end
 end
