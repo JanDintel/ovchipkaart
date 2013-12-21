cde5f28f71e1c62bd52411d03ecc4058e56754fb
Jan van der Pas <janvanderpas@gmail.com>
2013-12-21 02:20:48 +0100

    Refactor the configuration

--- lib/ovchipkaart.rb
+++ lib/ovchipkaart.rb
@@ -5,31 +5,46 @@ require 'ovchipkaart/parser'
 require 'ovchipkaart/api'
 
 module Ovchipkaart
+  class OvchipkaartError < StandardError; end
+  class ConfigurationError < OvchipkaartError; end
 
-  # Default config
-  @config = { username: 'foo', password: 'bar' }
+  # Configuration
+  MANDATORY_CONFIGURATION = { username: nil, password: nil }
+  CONFIGURATIONS_KEYS = MANDATORY_CONFIGURATION.merge!({})
 
-  @valid_config_keys = @config.keys
-
-  # Configure through hash
-  def self.configure(options = {})
-    options.each { |key, value| @config[key.to_sym] = value if @valid_config_keys.include? key.to_sym }
+  # Global config hash
+  @config = {}
+  def self.config
+    @config
   end
 
-  # Configure through yaml file
+  # Configuration with a hash
+  def self.configure(options = {})
+    MANDATORY_CONFIGURATION.each_key do |mandatory_option|
+      unless options.keys.map(&:to_sym).include?(mandatory_option)
+        raise ConfigurationError, 'Username and password are mandatory configuration options'
+      end
+    end
+
+    options.keys.map(&:to_sym).each do |given_configuration_key|
+      unless CONFIGURATIONS_KEYS.keys.include? given_configuration_key
+        raise ConfigurationError, 'Unknown configuration given'
+      end
+    end
+
+    options.each { |key, value| @config[key.to_sym] = value }
+  end
+
+  # Configuration with a YAML file
   def self.configure_with(path_to_yaml_file)
     begin
       config = YAML::load(IO.read(path_to_yaml_file))
     rescue Errno::ENOENT
-      log(:warning, "YAML configuration file couldn't be found. Using defaults."); return
+      raise ConfigurationError, 'YAML configuration file not found'
     rescue Psych::SyntaxError
-      log(:warning, "YAML configuration file contains invalid syntax. Using defaults."); return
+      raise ConfigurationError, 'YAML configuration file contains invalid syntax'
     end
 
     configure(config)
-  end
-
-  def self.config
-    @config
   end
 end
---
+++ spec/lib/ovchipkaart_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Ovchipkaart do
+
+  describe 'configuration' do
+    context 'valid configuration' do
+      specify { expect{ subject.configure({ username: 'foo', password: 'bar' }) }.not_to raise_error }
+
+      it 'sets the configuration' do
+        subject.configure({ username: 'foo', password: 'bar' })
+        expect(subject.config[:username]).to eql 'foo'
+        expect(subject.config[:password]).to eql 'bar'
+      end
+    end
+
+    context 'mandatory configuration' do
+      specify { expect(subject.const_get :MANDATORY_CONFIGURATION).to eql({:username=>nil, :password=>nil}) }
+
+      it 'raises an Ovchipkaart::ConfigurationError without the mandatory configuration' do
+        expect{ subject.configure({ foo: 'bar' }) }.to raise_error Ovchipkaart::ConfigurationError, 'Username and password are mandatory configuration options'
+      end
+    end
+
+    context 'configuration keys' do
+      it 'raises an Ovchipkaart::ConfigurationError with invalid configuration keys' do
+        expect{ subject.configure({ username: 'foo', password: 'bar', baz: 'foobar' }) }.to raise_error Ovchipkaart::ConfigurationError, 'Unknown configuration given'
+      end
+    end
+
+    context 'YAML configuration' do
+      it 'raises an Ovchipkaart::ConfigurationError without a YAML file' do
+        expect{ subject.configure_with('') }.to raise_error Ovchipkaart::ConfigurationError, 'YAML configuration file not found'
+      end
+
+      it 'raises an Ovchipkaart::ConfigurationError with an invalid YAML file' do
+        expect{ subject.configure_with('spec/fixtures/invalid.yml') }.to raise_error Ovchipkaart::ConfigurationError, 'YAML configuration file contains invalid syntax'
+      end
+    end
+  end
+end
