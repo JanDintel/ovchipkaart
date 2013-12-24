fb6ae6c4d35a94a39e599729a539611a68eff1b7
Jan van der Pas <janvanderpas@gmail.com>
2013-12-24 21:17:17 +0100

    Fix typo in the magic encoding line

--- lib/ovchipkaart.rb
+++ lib/ovchipkaart.rb
@@ -1,4 +1,4 @@
-# coding: utf-8
+# encoding: utf-8
 require 'ovchipkaart/setup'
 require 'ovchipkaart/version'
 require 'ovchipkaart/scraper'
--- lib/ovchipkaart/version.rb
+++ lib/ovchipkaart/version.rb
@@ -1,4 +1,4 @@
-# coding: utf-8
+# encoding: utf-8
 module Ovchipkaart
   VERSION = '0.0.1'
 end
